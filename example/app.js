bd87cc675751f938dcbd6f4587fb9caabdf78a91
plasmashadow <plasmashadowx@gmail.com>
2015-12-12 09:20:40 +0530

    adding few more bug fixes

--- example/app.js
+++ example/app.js
@@ -3,15 +3,15 @@ var minify = require('../index.js');
 
 var app = express();
 
-
-
-
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 
+
+app.use('/static', express.static(__dirname+"/static"));
 app.use(minify);
+
 app.get('/', function(req, res){
-   res.header("content-type", 'text/html')
+  //  res.header("content-type", 'text/html')
    res.render('index',{
      message: "hello"
    })
--- index.js
+++ index.js
@@ -1,23 +1,26 @@
-var express = require('express');
+/*
+* ProjectName: express-html-minify
+* author: plasmashadow
+*/
 var minify = require('html-minifier').minify;
-var interceptor = require('express-interceptor');
-
-
+var tamper = require('tamper');
+/*
+Default configuration using html-minfy
+holds the configuration object.
+ */
 var defaultConfig = {
   removeComments: true,
   removeCommentsFromCDATA: true,
   removeCDATASectionsFromCDATA: true,
   collapseWhitespace: true
-}
+};
 
-module.exports = interceptor(function(req, res) {
-  return {
-    isInterceptable: function() {
-      var flag = /text\/html/.test(res.get('Content-Type')) || !res.get('Content-Type');
-      return flag;
-    },
-    intercept: function(body, send) {
-      send(minify(body, defaultConfig));
-    }
+/*
+  Minify the body while processing the
+  response
+ */
+module.exports = tamper(function(req, res){
+  return function(body) {
+    return minify(body, defaultConfig);
   }
 });
